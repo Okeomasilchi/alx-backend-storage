111e0837925b097ce386536ee4a14ae35ba73d45
okeomasilchi <okeomasilachi@gmail.com>
2024-03-26 20:13:43 +0100

    Update topics for Holberton school

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+"""
+update_topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Function that changes all topics of a school
+    document based on the name
+    """
+    mongo_collection.update_many(
+      {"name": name},
+      {"$set": {"topics": topics}}
+      )
